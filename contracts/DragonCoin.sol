01c4b91f1d667c0bd0405fdcb95afba5b98b2e40
EthproblemSolver <156631998+EthproblemSolver@users.noreply.github.com>
2024-01-23 23:57:31 +0000

    Update DragonCoin.sol
    Done changes according to report

--- contracts/DragonCoin.sol
+++ contracts/DragonCoin.sol
@@ -1,33 +1,59 @@
 //SPDX-License-Identifier: MIT
-pragma solidity ^0.8.13;
-import "@openzeppelin/contracts/token/ERC20/presets/ERC20PresetMinterPauser.sol";
+pragma solidity ^0.8.20;
 import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Capped.sol";
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/utils/math/SafeMath.sol";
 
-contract DragonCoin is ERC20PresetMinterPauser, ERC20Capped, Ownable {
-    using SafeMath for uint256;
-    // Addresses for marketing and team wallets
-    address public marketingWallet = 0x4B20993Bc481177ec7E8f571ceCaE8A9e22C02db;
-    address public teamWallet = 0xAb8483F64d9C6d1EcF9b849Ae677dD3315835cb2; // Add your team wallet address here
+contract DragonCoin is ERC20, ERC20Capped, Ownable {
+    // Addresses
+    address public DEAD = 0x000000000000000000000000000000000000dEaD;
+    address private stakingWallet = 0xbF003d1988b6EE92cFaDDe09bA43E916B76a3A3C;
+    address private cexWallet = 0xF2556F64B17f72C8A2E80E1caD80eD6D4EeA7849;
+    address private lpWallet = 0xB6216226Ed1b188f517D0Ec47339163AD7cf9D22;
+    address private lpWalletOne = 0x5023ec088779AFd7569508F86DBDaDd839E8E02a;
+    address private p2eWalletOne = 0x1C8cC019dEAEf654Aeb3c8fb948A130aE2EacB22;
+    address private p2eWalletTwo = 0xdF51D96B5aB16c753D34c278E7E6185B1eD4E21C;
+    address private p2eWalletThree = 0x612f209901Fb0d5e3A0525b3b3d5F921288F56cE;
+    address private marketingWallet = 0x2C1bd5DF1F8F9C829f8Bbab0F7576ECEf12f65E9;
+    address private marketingWalletOne = 0xC66fe47981a8E65bFb3f18741b9742B00A76bCCC;
+    address private marketingWalletTwo = 0xd96541330657Fee4f27268577827320Feb20ebaE;
+    address private marketingWalletThree = 0xd17360C901faF0Fa9B671d219a60793073d283D1;
+    address private marketingWalletFour = 0xd0F1EA13970ECb65596f76B5a77870C870d66D00;
+    address private marketingWalletFive = 0xaaD141D497366BD299dcf98fC33bd65d544D2D5c;
+    address private marketingWalletSix = 0x3111F6930d768fCc2f336e82eB5D22ED281bD2Ee;
+    address private marketingWalletSev = 0x397123D20fBF39545F2E897D18af928859009859;
+    address private marketingWalletEig = 0x14444FbF063dF1456AB813d4b9588F4efC9848cA;
+    address private marketingWalletNine = 0xC89d320AfdC0B3305617268890c9E45b26404E74;
+    address private marketingWalletTen = 0xAb3DaF14BD077f34ca0746AC99ED1CF720503D85;
+    address private redistributionWallet = 0x25fF47A614e6D0BE79A5d61691E6eadD4a57Ed21;
+    
+    address public _pair;
 
-    // Total redistribution and LP balances
-    uint256 private _totalRedistributionBalance;
+    // Balances
     uint256 public lpBalance;
     uint256 public redistributionBalance;
 
+    // Tax Rates
     uint16 public buyTaxRate = 50; // 5% on buy
     uint16 public sellTaxRate = 50; // 5% on sell
     uint16 public transferTaxRate = 50; // 5% on transfer
     uint16 public maximumTaxPercentage = 100; // 10%
 
-    // Pair address for identifying buy, sell, or transfer
-    address private _pair;
+    // Flags
+    bool public taxesEnabled; // Flag to enable/disable taxes
+    bool private pairAddressSet = false;
 
-    // Flags to enable/disable taxes
-    bool public taxesEnabled = true; // Flag to enable/disable taxes
+    // Struct to hold tax percentages
+    struct Taxes {
+        uint16 marketing;
+        uint16 burn;
+        uint16 team;
+        uint16 redistribution;
+        uint16 lp;
+    }
 
-    //events
+    // Events
     event EtherWithdrawn(address indexed to, uint256 amount);
     event EtherDeposited(address indexed from, uint256 amount);
     event TaxRatesUpdated(
@@ -42,20 +68,11 @@ contract DragonCoin is ERC20PresetMinterPauser, ERC20Capped, Ownable {
         uint16 redistribution,
         uint16 lp
     );
-    event LpWithdrawn(uint256 indexed amount);
     event TaxesEnabled(string message);
     event TaxesDisabled(string message);
     event PairAddressUpdated(address indexed newPair);
 
-    // Struct to hold tax percentages
-    struct Taxes {
-        uint16 marketing;
-        uint16 burn;
-        uint16 team;
-        uint16 redistribution;
-        uint16 lp;
-    }
-
+    // Struct instance to hold tax percentages
     Taxes public transferTaxes = Taxes(300, 100, 300, 200, 100); // Initialize with default values (3%, 1%, 3%, 2%, 1%)
 
     // Mapping to exclude addresses from taxes
@@ -67,29 +84,57 @@ contract DragonCoin is ERC20PresetMinterPauser, ERC20Capped, Ownable {
     constructor(
         string memory tokenName,
         string memory tokenSymbol,
-        uint256 supply,
-        address beneficiary
-    )
-        ERC20PresetMinterPauser(tokenName, tokenSymbol)
-        ERC20Capped(SafeMath.mul(supply, 1 ether))
-    {
-        // _isExcludedFromTaxes[beneficiary] = true;
-        _mint(beneficiary, SafeMath.mul(supply, 1 ether));
-        // _mint(beneficiary, supply);
-    }
+        uint256 supply
+    ) ERC20(tokenName, tokenSymbol) ERC20Capped(supply * 1 ether) {
+        // Mint 80% of the tokens to LP/DEPLOYER address
+        uint256 lpTokens = (supply * 1 ether * 80) / 100;
+        _mint(0xB6216226Ed1b188f517D0Ec47339163AD7cf9D22, lpTokens);
 
-    function _beforeTokenTransfer(
-        address from,
-        address to,
-        uint256 amount
-    ) internal virtual override(ERC20, ERC20PresetMinterPauser) {
-        super._beforeTokenTransfer(from, to, amount);
+        // Mint 5% of the tokens to STAKING address
+        uint256 stakingTokens = (supply * 1 ether * 5) / 100;
+        _mint(stakingWallet, stakingTokens);
+
+        // Mint 4% of the tokens to CEX address
+        uint256 cexTokens = (supply * 1 ether * 4) / 100;
+        _mint(cexWallet, cexTokens);
+
+        // Mint 6% of the tokens to P2E address
+        uint256 p2eTokens = (supply * 1 ether * 5) / 100;
+        _mint(p2eWalletOne, p2eTokens);
+
+        uint256 remainingPercentage = (supply * 1 ether * 5) / 1000; // 0.5% as 5 / 1000
+
+        _mint(p2eWalletTwo, remainingPercentage);
+        _mint(p2eWalletThree, remainingPercentage);
+        _mint(marketingWalletOne, remainingPercentage);
+        _mint(marketingWalletTwo, remainingPercentage);
+        _mint(marketingWalletThree, remainingPercentage);
+        _mint(marketingWalletFour, remainingPercentage);
+        _mint(marketingWalletFive, remainingPercentage);
+        _mint(marketingWalletSix, remainingPercentage);
+        _mint(marketingWalletSev, remainingPercentage);
+        _mint(marketingWalletEig, remainingPercentage);
+        _mint(marketingWalletNine, remainingPercentage);
+        _mint(marketingWalletTen, remainingPercentage);
+
+        isExcludedFromTaxes(msg.sender);
+        isExcludedFromTaxes(_pair);
+        isExcludedFromTaxes(lpWallet);
+        isExcludedFromTaxes(lpWalletOne);
+        isExcludedFromTaxes(stakingWallet);
+        isExcludedFromTaxes(cexWallet);
+        isExcludedFromTaxes(marketingWallet);
+        isExcludedFromTaxes(p2eWalletOne);
+        isExcludedFromTaxes(p2eWalletTwo);
+        isExcludedFromTaxes(p2eWalletThree);
+
+        taxesEnabled = true;
     }
 
     function _transfer(
-        address sender,
-        address recipient,
-        uint256 amount
+    address sender,
+    address recipient,
+    uint256 amount
     ) internal virtual override(ERC20) {
         // Check if the sender is identified as a bot
         if (_isBot[sender]) {
@@ -101,65 +146,68 @@ contract DragonCoin is ERC20PresetMinterPauser, ERC20Capped, Ownable {
             // Calculate taxes
             uint256 taxAmount = 0;
 
-            // Determine the type of transfer (buy, sell, or regular transfer)
+            // Adjusted logic based on scenarios
             if (sender == _pair) {
-                // Buy tax
-                taxAmount = amount.mul(buyTaxRate).div(1000);
+                // Buy transaction
+                if (!_isExcludedFromTaxes[recipient]) {
+                    // Deduct buy taxes
+                    taxAmount = (amount * buyTaxRate) / 1000;
+                }
             } else if (recipient == _pair) {
-                // Sell tax
-                taxAmount = amount.mul(sellTaxRate).div(1000);
+                // Sell transaction
+                if (!_isExcludedFromTaxes[sender]) {
+                    // Deduct sell taxes
+                    taxAmount = (amount * sellTaxRate) / 1000;
+                }
             } else {
-                // Transfer tax
-                taxAmount = amount.mul(transferTaxRate).div(1000);
+                // Regular transfer
+                if (!_isExcludedFromTaxes[sender] && !_isExcludedFromTaxes[recipient]) {
+                    // Deduct transfer taxes
+                    taxAmount = (amount * transferTaxRate) / 1000;
+                }
             }
 
-            // Exclude certain addresses from taxes
-            if (
-                !_isExcludedFromTaxes[sender] &&
-                !_isExcludedFromTaxes[recipient]
-            ) {
+            // Continue with the rest of the logic when taxAmount > 0
+            if (taxAmount > 0) {
                 // Deduct tax amount from the transferred amount
-                uint256 netAmount = amount.sub(taxAmount);
-
-                // Update redistribution and LP balances
-                redistributionBalance = redistributionBalance.add(
-                    taxAmount.mul(transferTaxes.redistribution).div(1000)
-                );
-                lpBalance = lpBalance.add(
-                    taxAmount.mul(transferTaxes.lp).div(1000)
-                );
+                uint256 netAmount = amount - taxAmount;
 
                 // Distribute taxes
-                super._transfer(
-                    sender,
-                    teamWallet,
-                    taxAmount.mul(transferTaxes.team).div(1000)
-                );
-                super._transfer(
-                    sender,
-                    marketingWallet,
-                    taxAmount.mul(transferTaxes.marketing).div(1000)
-                );
-                super._transfer(
-                    sender,
-                    address(this),
-                    SafeMath.add(
-                        taxAmount.mul(transferTaxes.lp).div(1000),
-                        taxAmount.mul(transferTaxes.redistribution).div(1000)
-                    )
-                );
-                _burn(sender, taxAmount.mul(transferTaxes.burn).div(1000));
+                distributeTaxes(sender, taxAmount);
 
                 // Call parent implementation with the net amount after taxes
                 super._transfer(sender, recipient, netAmount);
-            } else {
-                // Call parent implementation with the original amount
-                super._transfer(sender, recipient, amount);
+                return;
             }
-        } else {
-            // If taxes are disabled, simply transfer the amount
-            super._transfer(sender, recipient, amount);
         }
+
+        // If taxes are disabled or no taxes to deduct, simply transfer the amount
+        super._transfer(sender, recipient, amount);
+    }
+
+    function distributeTaxes(address sender, uint256 taxAmount) private {
+        // Distribute taxes
+        uint256 redistribution = (taxAmount * transferTaxes.redistribution) / 1000;
+        super._transfer(sender, redistributionWallet, redistribution);
+
+        uint256 lpAmount = (taxAmount * transferTaxes.lp) / 1000;
+        super._transfer(sender, lpWalletOne, lpAmount);
+
+        address[5] memory teamWallets = [
+            0xe30828551bE2230cf6bfB39055D7557da4deb287,
+            0xe63351353B064D99c652F64F86D0121CFAC74eF1,
+            0x52f2D80c879C96209C4A7eB9b355a344ca6A132B,
+            0x36F83890173C68Af527e4d74D581873490E7A0BC,
+            0x04ae22013966860cf675C99AC43Ce613E2C5E30e
+        ];
+
+        uint256 teamShare = (taxAmount * transferTaxes.team) / 1000 / teamWallets.length;
+        for (uint256 i = 0; i < teamWallets.length; i++) {
+            super._transfer(sender, teamWallets[i], teamShare);
+        }
+
+        super._transfer(sender, marketingWallet, (taxAmount * transferTaxes.marketing) / 1000);
+        super._transfer(sender, DEAD, (taxAmount * transferTaxes.burn) / 1000);
     }
 
     function _mint(address account, uint256 amount)
@@ -211,21 +259,6 @@ contract DragonCoin is ERC20PresetMinterPauser, ERC20Capped, Ownable {
     }
 
     /**
-     * @dev Sets the bot status for multiple addresses. Only callable by the owner.
-     * @param accounts An array of addresses for which the bot status is to be set.
-     * @param state The new bot status to be set (true for bot, false for non-bot).
-     * @notice Bots may have restricted functionality depending on your implementation.
-     */
-    function bulkSetBot(address[] memory accounts, bool state)
-        external
-        onlyOwner
-    {
-        for (uint256 i = 0; i < accounts.length; i++) {
-            _isBot[accounts[i]] = state;
-        }
-    }
-
-    /**
      * @dev Checks if an address is identified as a bot.
      * @param account The address to check for bot status.
      * @return Whether the address is identified as a bot or not.
@@ -264,52 +297,17 @@ contract DragonCoin is ERC20PresetMinterPauser, ERC20Capped, Ownable {
             newPair != address(0),
             "DragonCoin: Pair address cannot be zero address"
         );
-        _pair = newPair;
-        emit PairAddressUpdated(newPair);
-    }
 
-    /**
-     * @dev Performs an airdrop to multiple recipients.
-     * @param recipients An array of addresses to receive the airdrop.
-     * @param amounts An array of amounts to be airdropped to each recipient.
-     * @notice The amounts array must have the same length as the recipients array.
-     */
-    function airdrop(address[] memory recipients, uint256[] memory amounts)
-        external
-        onlyOwner
-    {
+        // Ensure that the pair address can be set only once
         require(
-            recipients.length == amounts.length,
-            "Mismatched arrays length"
+            !pairAddressSet,
+            "DragonCoin: Pair address can be set only once"
         );
 
-        for (uint256 i = 0; i < recipients.length; i++) {
-            uint256 amount = amounts[i];
+        _pair = newPair;
+        pairAddressSet = true;
 
-            require(amount > 0, "Airdrop amount must be greater than 0");
-            require(
-                amount <= redistributionBalance,
-                "Insufficient redistribution balance"
-            );
-
-            // Perform airdrop
-            _transfer(address(this), recipients[i], amount);
-
-            // Update redistribution balance
-            redistributionBalance = redistributionBalance.sub(amount);
-        }
-    }
-
-    /**
-     * @dev Function to withdraw LP tokens. Only callable by the owner.
-     * @param amount The amount of LP tokens to be withdrawn.
-     * @notice LP tokens are typically associated with liquidity provision on decentralized exchanges.
-     */
-    function withdrawLp(uint256 amount) external onlyOwner {
-        require(amount <= lpBalance, "Insufficient LP balance");
-        lpBalance = lpBalance.sub(amount);
-        _transfer(address(this), owner(), amount);
-        emit LpWithdrawn(amount);
+        emit PairAddressUpdated(newPair);
     }
 
     /**
